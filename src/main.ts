0858703493ad27669ca31c45009573629c414b9a
RoccoCocco <rokokokeza@gmail.com>
2023-08-11 21:31:37 +0200

    chore: show package version in swagger

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { promises } from 'fs';
 
 import { AppModule } from '@/app.module';
 import { USER_AUTHENTICATION } from '@/http';
 
 async function bootstrap() {
+  const packageJson = await promises.readFile('./package.json');
+  const { version } = JSON.parse(packageJson.toString());
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .addBearerAuth({ type: 'http' }, USER_AUTHENTICATION)
     .setTitle('Book management')
-    .setVersion('1.0')
+    .setVersion(version ?? 'unknown')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, {
