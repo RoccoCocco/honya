fd40e6b517c32fe108598ee89180f40a78b59414
RoccoCocco <rokokokeza@gmail.com>
2023-08-13 12:47:04 +0200

    feat: health check

--- src/http/controllers/index.ts
+++ src/http/controllers/index.ts
@@ -1,3 +1,4 @@
 export * from './authentication.controller';
 export * from './book.controller';
+export * from './health.controller';
 export * from './user.controller';
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { UseCaseModule } from '@/usecase';
 
 import {
   AuthenticationController,
   BookController,
+  HealthController,
   UserController,
 } from './controllers';
 import { AuthenticationGuard } from './guards';
 
 @Module({
-  controllers: [AuthenticationController, UserController, BookController],
-  imports: [UseCaseModule],
+  controllers: [
+    AuthenticationController,
+    UserController,
+    BookController,
+    HealthController,
+  ],
+  imports: [TerminusModule, UseCaseModule],
   providers: [AuthenticationGuard],
 })
 export class HttpModule {}
